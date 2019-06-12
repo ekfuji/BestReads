daf260659632fd913b32b2dcef3bb4d6fb55f806
Raphael Lima Santana <raphaeltwins@hotmail.com>
2019-06-12 11:04:28 -0300

    Inicio da implementação do cadastrar livro

--- app/src/main/java/br/edu/ctup/bestreads/Activities/CadastrarLivroActivity.java
+++ app/src/main/java/br/edu/ctup/bestreads/Activities/CadastrarLivroActivity.java
@@ -3,11 +3,17 @@ package br.edu.ctup.bestreads.Activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import br.edu.ctup.bestreads.R;
 
 public class CadastrarLivroActivity extends AppCompatActivity {
+
+    private TextView txtNomeLivro, txtGeneroLivro, txtAutorLivro, txtAnoLivro;
+    private CheckBox checkboxLido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,22 @@ public class CadastrarLivroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastrar_livro);
         Intent getDados = getIntent();
         int idPasta = getDados.getIntExtra("idPasta",0);
+        txtNomeLivro =  findViewById(R.id.txt_nome_livro);
+        txtGeneroLivro = findViewById(R.id.txt_genero_livro);
+        txtAnoLivro = findViewById(R.id.txt_ano_publicacao_livro);
+        txtAutorLivro = findViewById(R.id.txt_autor_livro);
+        checkboxLido = findViewById(R.id.checkbox_lido);
         Toast.makeText(this, String.valueOf(idPasta), Toast.LENGTH_SHORT).show();
+    }
+
+    public void salvarLivro(View view) {
+
+    }
+
+    public void anexarImagem(View view) {
+    }
+
+    public void checkedLlido(View view) {
+
     }
 }
